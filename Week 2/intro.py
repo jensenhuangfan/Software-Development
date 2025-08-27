22d42a84a140bf5ac36af6c6b083af42ca913b01
jensenhuangfan <jensenhuangfan@gmail.com>
2025-08-27 10:02:45 -0400

    updated it for lowercased only ukraine

--- Week 2/intro.py
+++ Week 2/intro.py
@@ -7,11 +7,11 @@ print("Mr. Sekol is the coolest teacher in the universe aske Mr. Wright.")
 
 #Variables - These can change value based on user input or you can just assign a value to them
 
-player_name = "JFK"
+player_name = "Jeff"
 #concatention - combine strings + variables to form an output
 print("welcome "+player_name+" it's great to have you here.")
 print(player_name)
-player_name= "JFK KOed" #changed the name of the variable
+player_name= "JFK" #changed the name of the variable
 print("welcome ",player_name," it's great to have you here.") #you can use "," instead of "+" but the , makes more spacing
 
 #pass therough the vaiable into the output
@@ -19,9 +19,11 @@ print(f"Welcome, {player_name}, it's great to have you here.") # with the f in f
 
 #User Input - create a variable and ask user for input.
 
-country_of_origin = input(f"Well {player_name}, can you tell me the country you're from")
+country_of_origin = input(f"Well {player_name}, can you tell me the country you're from ")
 
 if country_of_origin == "Ukraine":
+    print(f"{player_name}, LMAO bros in the trenches")
+elif country_of_origin == "ukraine":
     print(f"{player_name}, LMAO bros in the trenches")
 
 else: print(f"{player_name} is from {country_of_origin}")
