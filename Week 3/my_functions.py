9f1680d82a337c44442cd084f0e366b1a50c7569
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-02 09:30:46 -0400

    made and ran my first function

--- Week 3/my_functions.py
+++ Week 3/my_functions.py
@@ -4,3 +4,8 @@ print("Fuctions Below")
 # How to Define a function
 # start with def
 # Indecation and spacing matters
+
+def sample_function():
+    print("You ran the fuction")
+
+sample_function()
