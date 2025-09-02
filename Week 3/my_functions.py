b7aa421e517c3556f1233ad8a41209b0aaef1169
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-02 09:43:17 -0400

    addition via function

--- Week 3/my_functions.py
+++ Week 3/my_functions.py
@@ -4,9 +4,20 @@ print("Fuctions Below")
 # How to Define a function
 # start with def
 # Indecation and spacing matters
-
+x = 10
+#call the sample function bellow
 def sample_function():
-    print("You ran the fuction")
-    sample_function()
+    print(f"{x} You ran the fuction")
+print(sample_function)
 
-sample_function()
+
+def do_addition(x, y):
+    sum = x + y
+    print(sum)
+
+do_addition(3,10)
+# Take input from the user and pass that value through the function
+num_1 = float(input("Please enter a number:"))
+num_2 = float(input("Please enter another number:"))
+
+do_addition(num_1, num_2)
