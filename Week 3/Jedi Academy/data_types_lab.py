fdc7f5b2383ceb3c42b752390d25e9aefe614334
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-04 09:33:12 -0400

    it workeds

---
+++ Week 3/Jedi Academy/data_types_lab.py
@@ -0,0 +1,34 @@
+my_int = 42
+my_float = 3.14
+print("The value of my_int is:", my_int)
+print("The value of my_float is:", my_float)
+
+addition = my_int + my_float
+subtraction = my_int - my_float
+multiplication = my_int * my_float
+division = my_int / my_float
+power = my_int ** 2
+modulus = my_int % 5
+floor_division = my_int // 5
+
+print("addition result:", addition)
+print("subtraction result:", subtraction)
+print("multiplication result:", multiplication)
+print("division result:", division)
+print("power result:", power)
+print("modulus result:", modulus)
+print("floor division result:", floor_division)
+
+first_name = "Luke"
+last_name = "Skywalker"
+full_name = first_name + " " + last_name
+print("Your Jedi name is:", full_name)
+
+name_length = len(full_name)
+print("The length of your Jedi name is:", name_length, "characters")
+
+message = "May the Force be with you, " + full_name + "!"
+print(message)
+
+starships = ["Millennium Falcon", "X-Wing", "TIE Fighter"]
+print("Available starships:", starships)
