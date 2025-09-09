f4f0a0b948c9c28eb97458b3aa7dfb3e3c55bb10
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-09 08:45:06 -0400

    i made a bank.

---
+++ Week 4/bankBalance.py
@@ -0,0 +1,13 @@
+customer_name = input("Welcome, what is your name? ")
+
+starting_balance = float(5000.25)
+
+print(f"Hello, {customer_name}, your starting balance is ${starting_balance}")
+pay_check = float(input("How much would you like to deposit? $"))
+expenditure_item= input("What did you buy? ")
+expenditure = float(input("How much does " + expenditure_item + " cost? $"))
+# def checking_balance (customer_name, starting_balance, pay_check, expenditure_item, expenditure):
+def checking_balance ():
+    ending_balance = starting_balance + pay_check - expenditure
+    print(f"Good day {customer_name} After spending money on a {expenditure_item}, in the amount of, {expenditure}, your current checking balance is: $, {ending_balance}")
+checking_balance()
