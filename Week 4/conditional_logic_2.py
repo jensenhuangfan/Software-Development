a95caab6dbc08fe720262ab8dd05e68017b48264
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-10 09:44:40 -0400

    logic update patch fix

--- Week 4/conditional_logic_2.py
+++ Week 4/conditional_logic_2.py
@@ -8,8 +8,24 @@ def check_credits():
 
         if numb_credits >= credits_to_gradutate:
             print(f"Congrates u smart!")
+            option_to_run_again()
         else:
               numb_short = credits_to_gradutate - numb_credits
               print(f"you dumb, u need {numb_short} to graduate.")
+              option_to_run_again()
+def option_to_run_again():        
+        run_again = input("Do you want to run again? y/n ")
+        if run_again == "y":
+              check_credits()
+        elif run_again == "n":
+              print("bye")
+        else:
+              run_again = input("Do you want to run again? y/n ")
+        if run_again == "y":
+              check_credits()
+        elif run_again == "n":
+              print("bye")
+        else:
+              print("U too stupid to graduate if u cant read!")
 
 check_credits()
