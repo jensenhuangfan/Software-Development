55f40bda3b3bc9453e3f59ed2aa7a1e4d1fbc3df
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-11 08:14:31 -0400

    made it nerfed so it only works in penny values

--- Week 4/change_calculator.py
+++ Week 4/change_calculator.py
@@ -1,59 +1,54 @@
-og_money = int(input("Give me monies no change yet $"))
+og_money = int(input("Give me monies in the value of pennys, IE. $1.24 = ¢124, enter here: ¢"))
 money = og_money
 
-hundred_bills = money / 100
+hundred_bills = money / 10000
 hundred_bills = int(hundred_bills)
-money = money - hundred_bills * 100
+money = money - hundred_bills * 10000
 
-fifty_bills =  money/50
+fifty_bills =  money/5000
 fifty_bills = int(fifty_bills)
-money = money - fifty_bills * 50
+money = money - fifty_bills * 5000
 
-twenty_bills =  money/20
+twenty_bills =  money/2000
 twenty_bills = int(twenty_bills)
-money = money - twenty_bills * 20
+money = money - twenty_bills * 2000
 
-ten_bills =  money/10
+ten_bills =  money/1000
 ten_bills = int(ten_bills)
-money = money - ten_bills * 10
+money = money - ten_bills * 1000
 
-five_bills =  money/5
+five_bills =  money/500
 five_bills = int(five_bills)
-money = money - five_bills * 5
+money = money - five_bills * 500
 
-two_bills =  money/2
+two_bills =  money/200
 two_bills = int(two_bills)
-money = money - two_bills * 2
+money = money - two_bills * 200
 
-one_bills =  money/1
+one_bills =  money/100
 one_bills = int(one_bills)
-money = money - one_bills * 1
+money = money - one_bills * 100
 
-print(hundred_bills, fifty_bills, twenty_bills, ten_bills, five_bills, two_bills, one_bills)
-
-og_cents = int(input("Give me cents no dollars ¢"))
-cents = og_cents
-
-half_dollar =  cents/50
+half_dollar =  money/50
 half_dollar = int(half_dollar)
-cents = cents - half_dollar * 50
+money = money - half_dollar * 50
 
-quarter =  cents/25
+quarter =  money/25
 quarter = int(quarter)
-cents = cents - quarter * 25
+money = money - quarter * 25
 
-dimes =  cents/10
+dimes =  money/10
 dimes = int(dimes)
-cents = cents - dimes * 10
+money = money - dimes * 10
 
-nickle =  cents/5
+nickle =  money/5
 nickle = int(nickle)
-cents = cents - nickle * 5
+money = money - nickle * 5
 
-pennies =  cents/1
+pennies =  money/1
 pennies = int(pennies)
-cents = cents - pennies * 1
+money = money - pennies * 1
 
 print(half_dollar, quarter, dimes, nickle, pennies)
 
-print(f"Change for ${og_money}.{og_cents} is {hundred_bills} One Hundred dollar bills, {fifty_bills} fifty dollar bills, {twenty_bills} twenty dollar bills, {ten_bills} ten dollar bills, {five_bills} five dollar bills, {two_bills} two dollar bills, {one_bills} one dollar bills, {half_dollar} half dollar coins, {quarter} quarters, {dimes} dimes, {nickle} nickles, and {pennies} pennies.")
+print(f"Change for ¢{og_money} is {hundred_bills} One Hundred dollar bills, {fifty_bills} fifty dollar bills, {twenty_bills} twenty dollar bills, {ten_bills} ten dollar bills, {five_bills} five dollar bills, {two_bills} two dollar bills, {one_bills} one dollar bills, {half_dollar} half dollar coins, {quarter} quarters, {dimes} dimes, {nickle} nickles, and {pennies} pennies.")
