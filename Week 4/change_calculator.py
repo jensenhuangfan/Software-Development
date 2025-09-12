864039a8b6dd60bb1eed4e43248c4e5443881499
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-12 09:32:52 -0400

    Code is efficent by over 33%

--- Week 4/change_calculator.py
+++ Week 4/change_calculator.py
@@ -1,40 +1,40 @@
 og_money = int(input("Give me monies in the value of pennys, IE. $1.24 = ¢124, enter here: ¢"))
 money = og_money
 
-hundred_bills = money // 10000
+hundred_bills = money // 10000 #Double // is floor division
 money = money - hundred_bills * 10000
 
-fifty_bills =  money//5000
+fifty_bills =  money//5000 #Double // is floor division
 money = money - fifty_bills * 5000
 
-twenty_bills =  money//2000
+twenty_bills =  money//2000 #Double // is floor division 
 money = money - twenty_bills * 2000
 
-ten_bills =  money//1000
+ten_bills =  money//1000 #Double // is floor division
 money = money - ten_bills * 1000
 
-five_bills =  money//500
+five_bills =  money//500 #Double // is floor division
 money = money - five_bills * 500
 
-two_bills =  money//200
+two_bills =  money//200 #Double // is floor division
 money = money - two_bills * 200
 
-one_bills =  money//100
+one_bills =  money//100 #Double // is floor division
 money = money - one_bills * 100
 
-half_dollar =  money//50
+half_dollar =  money//50 #Double // is floor division
 money = money - half_dollar * 50
 
-quarter =  money//25
+quarter =  money//25 #Double // is floor division
 money = money - quarter * 25
 
-dimes =  money//10
+dimes =  money//10 #Double // is floor division
 money = money - dimes * 10
 
-nickle =  money//5
+nickle =  money//5 #Double // is floor division
 money = money - nickle * 5
 
-pennies =  money//1
+pennies =  money//1 #Double // is floor division
 money = money - pennies * 1
 
 print(f"Change for ¢{og_money} is {hundred_bills} One Hundred dollar bills, {fifty_bills} fifty dollar bills, {twenty_bills} twenty dollar bills, {ten_bills} ten dollar bills, {five_bills} five dollar bills, {two_bills} two dollar bills, {one_bills} one dollar bills, {half_dollar} half dollar coins, {quarter} quarters, {dimes} dimes, {nickle} nickles, and {pennies} pennies.")
