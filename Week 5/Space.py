db7d99ca225a25205df96c68e95c8f3a3bb800c3
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-16 09:36:38 -0400

    FEATURE UPDATE, added ability to run again

--- Week 5/Space.py
+++ Week 5/Space.py
@@ -1,24 +1,27 @@
-def weight_convertion():
-    name = input("What's your name boxer? ")
-    weight = float(input(f"{name}, how much do you weigh in LB? DONT USE LETTERS IT WILL FAIL! "))
-    mercury_weight = weight / 100 * 90
-    venus_weight  = weight / 100 * 91
-    mars_weight = weight / 100 * 38
-    jupiter_weight = weight / 100 * 234
-    saturn_weight  = weight / 100 * 106
-    uranus_weight  = weight / 100 * 92
-    neptune_weight = weight / 100 * 119
-    pluto_weight = weight / 100 * 6.3
+autorun = True
+while autorun:
+      name = input("What's your name boxer? ")
+      weight = float(input(f"{name}, how much do you weigh in LB? DONT USE LETTERS IT WILL FAIL! "))
+      mercury_weight = weight / 100 * 90
+      venus_weight  = weight / 100 * 91
+      mars_weight = weight / 100 * 38
+      jupiter_weight = weight / 100 * 234
+      saturn_weight  = weight / 100 * 106
+      uranus_weight  = weight / 100 * 92
+      neptune_weight = weight / 100 * 119
+      pluto_weight = weight / 100 * 6.3
 
-    print(f"""
-     {name} here are your weights in each of the planets in the solar system
-     Mercury Weight = {mercury_weight}LB
-     Venus Weight = {venus_weight}LB
-     Mars Weight = {mars_weight}LB
-     Jupiter Weight = {jupiter_weight}LB
-     Saturn Weight = {saturn_weight}LB
-     Uranus Weight = {uranus_weight}LB
-     Neptune Weight = {neptune_weight}LB
-     Pluto Weight = {pluto_weight}LB""")
+      print(f"""{name} here are your weights in each of the planets in the solar system
+      Mercury Weight = {mercury_weight}LB
+      Venus Weight = {venus_weight}LB
+      Mars Weight = {mars_weight}LB
+      Jupiter Weight = {jupiter_weight}LB
+      Saturn Weight = {saturn_weight}LB
+      Uranus Weight = {uranus_weight}LB
+      Neptune Weight = {neptune_weight}LB
+      Pluto Weight = {pluto_weight}LB""")
 
-weight_convertion()
+      run_again = input("Do you want to run again? y/n ")
+      if run_again != "y":
+            autorun = False
+            print(f"Have a good day {name}")
