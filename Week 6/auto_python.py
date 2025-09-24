ecb75a3adcff81f4bc59f32812bf73b4b793d559
jensenhuangfan <jensenhuangfan@gmail.com>
2025-09-24 08:16:13 -0400

    it works well

--- Week 6/auto_python.py
+++ Week 6/auto_python.py
@@ -25,25 +25,38 @@ def pointer_location():
 # pointer_location()
 
 def send_email():
-    EMAIL_URL = "https://mail.google.com"
-    EMAIL_TO = "michael.sekol@mahoningctc.com"
-    SUBJECT = "Hi"
-    BODY = "hi"
+    EMAIL_URL = "sender"
+    EMAIL_TO = "resipeint"
+    SUBJECT = "subject"
+    BODY = "my body is ready"
     jfk.moveTo(1250, 1051)
     time.sleep(.05)
     jfk.click
     jfk.moveTo(1250, 64)
+    jfk.click(1250, 64)
     time.sleep(.05)
-    jfk.click
-    jfk.click
-    jfk.click
     jfk.sleep(.1)
     jfk.write(EMAIL_URL)
     time.sleep(1)
     jfk.press("enter")
     jfk.sleep(3)
-    jfk.moveTo(1120, 620)
+    jfk.moveTo(157, 188)
     time.sleep(.05)
-    jfk.click
+    jfk.click(157, 188)
+    jfk.moveTo(1494, 469)
+    time.sleep(.05)
+    jfk.click(1494, 469)
+    jfk.write(EMAIL_TO)
+    jfk.moveTo(1284, 510)
+    time.sleep(.05)
+    jfk.click(1284, 510)
+    jfk.write(SUBJECT)
+    jfk.moveTo(1284, 832)
+    time.sleep(.05)
+    jfk.click(1284, 832)
+    jfk.write(BODY)
+    jfk.moveTo(1322, 999)
+    time.sleep(.05)
+    jfk.click(1322, 999)
 
 send_email()
